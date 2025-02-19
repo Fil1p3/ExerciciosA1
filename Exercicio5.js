ae53d618b83173ffd8a1d464b0c70143e8299d1e
Fil1p3 <164559658+Fil1p3@users.noreply.github.com>
2025-02-19 15:28:19 -0300

    Exercicio5.js

--- Exercicio5.js
+++ Exercicio5.js
@@ -1,12 +1,10 @@
-const readline = require("readline"); // importa o módulo readline
+const readline = require("readline"); 
 
-// cria uma interface de leitura para entrada e saída de dados
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// pedindo para que o usuário informe seu nome e captura o dado
 rl.question("Digite a 1° nota: ", (n1) => {
 
   rl.question("Digite a 2° nota  ", (n2) => {
@@ -15,6 +13,6 @@ rl.question("Digite a 1° nota: ", (n1) => {
     const media = parseInt(soma) /2;
     console.log(`Resultado: ${media}`);
     
-    rl.close(); // fecha a interface após a captura dos dados
+    rl.close();
   });
 });
