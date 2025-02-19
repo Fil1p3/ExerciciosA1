3b3bb8bfe146822a2f8cf082a2a114a20baffa3e
Fil1p3 <164559658+Fil1p3@users.noreply.github.com>
2025-02-19 15:28:35 -0300

    Exercicio6.js

--- Exercicio6.js
+++ Exercicio6.js
@@ -1,12 +1,11 @@
-const readline = require("readline"); // importa o módulo readline
+const readline = require("readline"); 
 
-// cria uma interface de leitura para entrada e saída de dados
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// pedindo para que o usuário informe seu nome e captura o dado
+
 rl.question("Digite um Numero: ", (n) => {
 
   const sucessor = parseInt(n)+1
@@ -15,6 +14,6 @@ rl.question("Digite um Numero: ", (n) => {
     console.log(`Seu sucessor é: ${sucessor}`);
     console.log(`Seu antecessor é: ${antecessor}`);
     
-    rl.close(); // fecha a interface após a captura dos dados
+    rl.close(); 
   });
 
