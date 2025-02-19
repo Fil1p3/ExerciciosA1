cf553f1f7e349e5559f0e7e2a09d1f516aee7bde
Fil1p3 <164559658+Fil1p3@users.noreply.github.com>
2025-02-19 15:28:56 -0300

    Exercicio7.js

--- Exercicio7.js
+++ Exercicio7.js
@@ -1,18 +1,16 @@
-const readline = require("readline"); // importa o módulo readline
+const readline = require("readline");
 
-// cria uma interface de leitura para entrada e saída de dados
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// pedindo para que o usuário informe seu nome e captura o dado
 rl.question("Quanto voce tem na sua carteria hoje em reais? ", (n) => {
 
   const dolar = parseFloat(n)/5.50
 
     console.log(`Com esse valor, voce pode comprar ${dolar} dolares`);
   
-    rl.close(); // fecha a interface após a captura dos dados
+    rl.close();
   });
 
